8ba3526e8e9a91a282be4280b7b0da6ab57413f2
Adam Dłubak <adam.dlubak@gmail.com>
2017-12-31 00:50:41 +0100

    Added example styles

--- PersonDataWizard/obj/Debug/PersonDataWizard_Content.g.i.cs
+++ PersonDataWizard/obj/Debug/PersonDataWizard_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("photo.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("favicon.ico")]
 
 
